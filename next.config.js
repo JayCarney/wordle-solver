c0995df30693f58fcd7deefe303c4eb3144b1aae
JayCarney <jason.jay.carney@gmail.com>
2022-01-23 15:27:18 -0600

    feat: adding analytics

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const isProd = process.env.NODE_ENV === 'production'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/wordle-solver/' : ''
+  assetPrefix: isProd ? '/wordle-solver/' : '',
+  env: {
+    NEXT_PUBLIC_GOOGLE_ANALYTICS: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS
+  }
 }
 
 module.exports = nextConfig
