2be2ee46b1384fe406edce499a9e693a81043365
JayCarney <jason.jay.carney@gmail.com>
2022-01-23 17:56:53 +0000

    Deploying to gh-pages from @ JayCarney/wordle-solver@f2bcc460fc61525f279257256aa0bbaaaf3aed33 🚀

---
+++ _next/static/8dhmV9dhNzjFkb0W5GjJx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/158-7cec7b6c99ad2144.js","static/chunks/pages/index-e3996796c010f904.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
