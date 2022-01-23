d987b6f9ace1e20171cc7194a535750ad438d3fb
JayCarney <jason.jay.carney@gmail.com>
2022-01-23 12:45:44 -0600

    fix: github pages path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? '/wordle-solver/' : ''
 }
 
 module.exports = nextConfig
