a9694499d921cffa14e0b8d4536902c8e4222e99
JayCarney <jason.jay.carney@gmail.com>
2022-01-27 20:41:43 -0600

    fix: positional letter score only counts once now

--- utils/score-word.spec.ts
+++ utils/score-word.spec.ts
@@ -2,6 +2,12 @@ import { scoreWord } from "./score-word"
 
 describe('scoreWord', () => {
   test('Returns count of letter scores', () => {
-    expect(scoreWord('te', [{t: 2},{}])).toBe(2)
+    expect(scoreWord('te', {inWord: {t: 1, e: 1}, positional: [{t: 1}, {e: 1}]})).toBe(6)
+  })
+  test('Only scores the first instance of a word match by letter', () => {
+    expect(scoreWord('tt', {inWord: {t: 1}, positional: []})).toBe(1)
+  })
+  test('Only scores the highest instance of a word match by position', () => {
+    expect(scoreWord('tt', {inWord: {}, positional: [{t: 1}, {t: 2}]})).toBe(4)
   })
 })
--- utils/score-word.ts
+++ utils/score-word.ts
@@ -1,24 +1,45 @@
 import { Guess } from "./create-guess-from-word";
 import { LetterScores } from "./generate-letter-scores";
-import wordsDict from '../wordsDict.json'
-import * as R from 'ramda'
+import wordsDict from "../wordsDict.json";
+import * as R from "ramda";
 
-const MATCH_POSITION_MULTIPLIER = 2
-const MATCH_OTHER_WORDS_MULTIPLIER = 1
-const MATCH_POSSIBLE_ANSWER_POINTS = 20
+const MATCH_POSITION_MULTIPLIER = 2;
+const MATCH_OTHER_WORDS_MULTIPLIER = 1;
+const MATCH_POSSIBLE_ANSWER_POINTS = 20;
 
-export function scoreWord(word: string, letterScores: LetterScores, guesses: Guess[] = []): number {
-  const letterArray = word.split('')
-  let charScore = 0
-  // positional score
-  charScore += letterArray.reduce((score, letter, letterIndex) => {
-    return score + R.pathOr(0, ['positional', letterIndex, letter], letterScores) * MATCH_POSITION_MULTIPLIER
-  }, 0)
+export function scoreWord(
+  word: string,
+  letterScores: LetterScores,
+  guesses: Guess[] = []
+): number {
+  const letterArray = word.split("");
+  let charScore = 0;
+  // positional score, highest only
+  charScore = Object.values(
+    letterArray.reduce((scores, letter, letterIndex) => {
+      return R.assocPath(
+        [letter],
+        Math.max(
+          R.pathOr(0, [letter], scores),
+          R.pathOr(0, ["positional", letterIndex, letter], letterScores) *
+            MATCH_POSITION_MULTIPLIER
+        ),
+        scores
+      );
+    }, {} as Record<string, number>)
+  ).reduce((score, letterScore) => score + letterScore, 0);
+
   // matching words score
   charScore += R.uniq(letterArray).reduce((score, letter) => {
-    return score + R.pathOr(0, ['inWord', letter], letterScores) * MATCH_OTHER_WORDS_MULTIPLIER
-  }, 0)
+    return (
+      score +
+      R.pathOr(0, ["inWord", letter], letterScores) *
+        MATCH_OTHER_WORDS_MULTIPLIER
+    );
+  }, 0);
   // is a possible answer
-  charScore += wordsDict.answers.includes(word) ? MATCH_POSSIBLE_ANSWER_POINTS : 0
-  return charScore
+  charScore += wordsDict.answers.includes(word)
+    ? MATCH_POSSIBLE_ANSWER_POINTS
+    : 0;
+  return charScore;
 }
